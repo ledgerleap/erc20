c8c1a3cfabfc485caaa3651162345b1065d0e217
= <=>
2021-04-01 17:00:29 -0400

    added pub fn

--- tests/src/erc20.rs
+++ tests/src/erc20.rs
@@ -2,140 +2,166 @@ use casper_engine_test_support::{Code, Hash, SessionBuilder, TestContext, TestCo
 use casper_types::{CLTyped, PublicKey, RuntimeArgs, U256, U512, account::AccountHash, bytesrepr::FromBytes, runtime_args};
 
 pub mod token_cfg {
-    use super::*;
-    pub const NAME: &str = "ERC20";
-    pub const SYMBOL: &str = "STX";
-    pub const DECIMALS: u8 = 18;
-    pub fn total_supply() -> U256 {
-        1_000.into()
-    }
+	use super::*;
+	pub const NAME: &str = "ERC20";
+	pub const SYMBOL: &str = "STX";
+	pub const DECIMALS: u8 = 18;
+	pub fn total_supply() -> U256 {
+		1_000.into()
+	}
 }
 
 pub struct Sender(pub AccountHash);
 
 pub struct Token {
-    context: TestContext,
-    pub ali: AccountHash,
-    pub bob: AccountHash,
-    pub joe: AccountHash
+	context: TestContext,
+	pub ali: AccountHash,
+	pub bob: AccountHash,
+	pub joe: AccountHash
 }
 
 impl Token {
-    pub fn deployed() -> Token {
-        let ali = PublicKey::ed25519([3u8; 32]).unwrap();
-        let bob = PublicKey::ed25519([6u8; 32]).unwrap();
-        let joe = PublicKey::ed25519([9u8; 32]).unwrap();
+	pub fn deployed() -> Token {
+		let ali = PublicKey::ed25519([3u8; 32]).unwrap();
+		let bob = PublicKey::ed25519([6u8; 32]).unwrap();
+		let joe = PublicKey::ed25519([9u8; 32]).unwrap();
 
-        let mut context = TestContextBuilder::new()
-            .with_public_key(ali, ali.to_account_hash(), U512::from(500_000_000_000_000_000u64))
-            .with_public_key(bob, bob.to_account_hash(), U512::from(500_000_000_000_000_000u64))
-            .build();
-        let session_code = Code::from("contract.wasm");
-        let session_args = runtime_args! {
-            "tokenName" => token_cfg::NAME,
-            "tokenSymbol" => token_cfg::SYMBOL,
-            "tokenTotalSupply" => token_cfg::total_supply()
-        };
-        let session = SessionBuilder::new(session_code, session_args)
-            .with_address(ali.to_account_hash())
-            .with_authorization_keys(&[ali.to_account_hash()])
-            .build();
-        context.run(session);
-        Token { context, ali: ali.to_account_hash(), bob: bob.to_account_hash(), joe: joe.to_account_hash() }
-    }
+		let mut context = TestContextBuilder::new()
+			.with_public_key(ali, ali.to_account_hash(), U512::from(500_000_000_000_000_000u64))
+			.with_public_key(bob, bob.to_account_hash(), U512::from(500_000_000_000_000_000u64))
+			.build();
+		let session_code = Code::from("contract.wasm");
+		let session_args = runtime_args! {
+			"tokenName" => token_cfg::NAME,
+			"tokenSymbol" => token_cfg::SYMBOL,
+			"tokenTotalSupply" => token_cfg::total_supply()
+		};
+		let session = SessionBuilder::new(session_code, session_args)
+			.with_address(ali.to_account_hash())
+			.with_authorization_keys(&[ali.to_account_hash()])
+			.build();
+		context.run(session);
+		Token { context, ali: ali.to_account_hash(), bob: bob.to_account_hash(), joe: joe.to_account_hash() }
+	}
 
-    fn contract_hash(&self) -> Hash {
-        self.context
-            .query(self.ali, &[format!("{}_hash", token_cfg::NAME)])
-            .unwrap_or_else(|_| panic!("{} contract not found", token_cfg::NAME))
-            .into_t()
-            .unwrap_or_else(|_| panic!("{} has wrong type", token_cfg::NAME))
-    }
+	pub fn add_whitelist(
+		&mut self, 
+		sender: Sender
+	) {
+		self.call(
+			sender, 
+			"addWhitelist",
+			runtime_args! {},
+		);
+	}
 
-    fn query_contract<T: CLTyped + FromBytes>(&self, name: &str) -> Option<T> {
-        match self
-            .context
-            .query(self.ali, &[token_cfg::NAME.to_string(), name.to_string()])
-        {
-            Err(_) => None,
-            Ok(maybe_value) => {
-                let value = maybe_value
-                    .into_t()
-                    .unwrap_or_else(|_| panic!("{} is not expected type.", name));
-                Some(value)
-            }
-        }
-    }
+	pub fn remove_whitelist(
+		&mut self, 
+		sender: Sender
+	) {
+		self.call(
+			sender, 
+			"removeWhitelist",
+			runtime_args! {},
+		);
+	}
 
-    fn call(&mut self, sender: Sender, method: &str, args: RuntimeArgs) {
-        let Sender(address) = sender;
-        let code = Code::Hash(self.contract_hash(), method.to_string());
-        let session = SessionBuilder::new(code, args)
-            .with_address(address)
-            .with_authorization_keys(&[address])
-            .build();
-        self.context.run(session);
-    }
+	pub fn get_whitelist(&self) -> i32 {
+		self.query_contract("_whitelist_size").unwrap();
+	}
 
-    pub fn name(&self) -> String {
-        self.query_contract("_name").unwrap()
-    }
+	fn contract_hash(&self) -> Hash {
+		self.context
+			.query(self.ali, &[format!("{}_hash", token_cfg::NAME)])
+			.unwrap_or_else(|_| panic!("{} contract not found", token_cfg::NAME))
+			.into_t()
+			.unwrap_or_else(|_| panic!("{} has wrong type", token_cfg::NAME))
+	}
 
-    pub fn symbol(&self) -> String {
-        self.query_contract("_symbol").unwrap()
-    }
+	fn query_contract<T: CLTyped + FromBytes>(&self, name: &str) -> Option<T> {
+		match self
+			.context
+			.query(self.ali, &[token_cfg::NAME.to_string(), name.to_string()])
+		{
+			Err(_) => None,
+			Ok(maybe_value) => {
+				let value = maybe_value
+					.into_t()
+					.unwrap_or_else(|_| panic!("{} is not expected type.", name));
+				Some(value)
+			}
+		}
+	}
 
-    pub fn decimals(&self) -> u8 {
-        self.query_contract("_decimals").unwrap()
-    }
+	fn call(&mut self, sender: Sender, method: &str, args: RuntimeArgs) {
+		let Sender(address) = sender;
+		let code = Code::Hash(self.contract_hash(), method.to_string());
+		let session = SessionBuilder::new(code, args)
+			.with_address(address)
+			.with_authorization_keys(&[address])
+			.build();
+		self.context.run(session);
+	}
 
-    pub fn balance_of(&self, account: AccountHash) -> U256 {
-        let key = format!("_balances_{}", account);
-        self.query_contract(&key).unwrap_or_default()
-    }
+	pub fn name(&self) -> String {
+		self.query_contract("_name").unwrap()
+	}
 
-    pub fn allowance(&self, owner: AccountHash, spender: AccountHash) -> U256 {
-        let key = format!("_allowances_{}_{}", owner, spender);
-        self.query_contract(&key).unwrap_or_default()
-    }
+	pub fn symbol(&self) -> String {
+		self.query_contract("_symbol").unwrap()
+	}
 
-    pub fn transfer(&mut self, recipient: AccountHash, amount: U256, sender: Sender) {
-        self.call(
-            sender,
-            "transfer",
-            runtime_args! {
-                "recipient" => recipient,
-                "amount" => amount
-            },
-        );
-    }
+	pub fn decimals(&self) -> u8 {
+		self.query_contract("_decimals").unwrap()
+	}
 
-    pub fn approve(&mut self, spender: AccountHash, amount: U256, sender: Sender) {
-        self.call(
-            sender,
-            "approve",
-            runtime_args! {
-                "spender" => spender,
-                "amount" => amount
-            },
-        );
-    }
+	pub fn balance_of(&self, account: AccountHash) -> U256 {
+		let key = format!("_balances_{}", account);
+		self.query_contract(&key).unwrap_or_default()
+	}
 
-    pub fn transfer_from(
-        &mut self,
-        owner: AccountHash,
-        recipient: AccountHash,
-        amount: U256,
-        sender: Sender,
-    ) {
-        self.call(
-            sender,
-            "transferFrom",
-            runtime_args! {
-                "owner" => owner,
-                "recipient" => recipient,
-                "amount" => amount
-            },
-        );
-    }
+	pub fn allowance(&self, owner: AccountHash, spender: AccountHash) -> U256 {
+		let key = format!("_allowances_{}_{}", owner, spender);
+		self.query_contract(&key).unwrap_or_default()
+	}
+
+	pub fn transfer(&mut self, recipient: AccountHash, amount: U256, sender: Sender) {
+		self.call(
+			sender,
+			"transfer",
+			runtime_args! {
+				"recipient" => recipient,
+				"amount" => amount
+			},
+		);
+	}
+
+	pub fn approve(&mut self, spender: AccountHash, amount: U256, sender: Sender) {
+		self.call(
+			sender,
+			"approve",
+			runtime_args! {
+				"spender" => spender,
+				"amount" => amount
+			},
+		);
+	}
+
+	pub fn transfer_from(
+		&mut self,
+		owner: AccountHash,
+		recipient: AccountHash,
+		amount: U256,
+		sender: Sender,
+	) {
+		self.call(
+			sender,
+			"transferFrom",
+			runtime_args! {
+				"owner" => owner,
+				"recipient" => recipient,
+				"amount" => amount
+			},
+		);
+	}
 }
