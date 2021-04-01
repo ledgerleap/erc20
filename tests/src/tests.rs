0375e7e5b87bf0de66b3f559a12a4efd369243a0
= <=>
2021-04-01 15:26:54 -0400

    test4

--- tests/src/tests.rs
+++ tests/src/tests.rs
@@ -67,7 +67,6 @@ fn test_erc20_transfer_from_too_much() {
 
 #[test]
 fn testvarstring() {
-    // let testvar: &str = "test var string";
-    // println!("{:?}", testvar);
-    println!(" ---- test var string ---- ");
+    let testvar: &str = "test var string";
+    println!("{:?}", testvar);
 }
